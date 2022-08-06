4e2e8afad6ef16ae179d0009fba55a56f3c33b1b
Kailin <kailinx@kailinx.com>
2022-08-06 17:57:06 +0200

    update

--- SmarterTomorrowST.py
+++ SmarterTomorrowST.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import wordcloud
 from wordcloud import WordCloud
 from wordcloud import ImageColorGenerator
